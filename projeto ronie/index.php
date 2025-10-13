b7ef05ae56bcb97c18e548b619ea46140d4bb874
Ferrasz <111441434+FerraszXD@users.noreply.github.com>
2025-10-13 19:47:59 -0300

    Add files via upload

---
+++ projeto ronie/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- <meta http-equiv="refresh" content="3; url=./view-login.php"> -->
+    <title>Login Usuário</title>
+    <link rel="stylesheet" href="./css/estilo.css">
+
+    <style>
+        .container-redirecionar{
+            width: 100vw;
+            height: 100vh;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            flex-direction: column;            
+        }
+
+        h1{
+            font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+            font-weight: 100;
+            font-size: 3rem;
+            padding-bottom: 50px;
+        }
+    </style>
+</head>
+<body>
+
+<h1>CATALOGAO DE FILMASSOS</h1>
+
+    
+       <button onclick="window.location.href='catalogo.php'">ENTRAR</button>
+
+</body>
+</html>
